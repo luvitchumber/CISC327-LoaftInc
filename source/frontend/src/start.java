dbde566e6baef2ecb7da368cffcd2846e938bc75
luvitchumber <luvitchumber@gmail.com>
2019-10-16 13:14:52 -0400

    pushing classes to source code

---
+++ source/frontend/src/start.java
@@ -0,0 +1,12 @@
+
+public class start {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+// use this to call front end from cli with files
+		// call login to start instance
+		//verify
+		//call frontend now with parameters to create instance loop
+	}
+
+}
