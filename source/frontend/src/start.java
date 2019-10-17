4a4bcd1fd0adc06e4ec33a540850756fd725bdec
luvitchumber <luvitchumber@gmail.com>
2019-10-17 16:20:57 -0400

    retructure order

--- source/frontend/src/start.java
+++ source/frontend/src/start.java
@@ -4,9 +4,10 @@ public class start {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 // use this to call front end from cli with files
-		// call login to start instance
+		
 		//verify
 		//call frontend now with parameters to create instance loop
+		// call login to start instance
 	}
 
 }
