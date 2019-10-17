1561f12fdb14f17729b02637d2d2f031d26258bf
15sm3 <15sm3@queensu.ca>
2019-10-17 18:38:48 -0400

    yeet

--- source/frontend/src/functions/deposit.java
+++ source/frontend/src/functions/deposit.java
@@ -1,5 +1,25 @@
 package functions;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class deposit {
+	
+	public int depositToAccount(Terminal terminal, String account, double amount) {
+			
+			//read in valid accounts
+			//prompt user to input acct number
+				//read inputted account
+					//check if valid
+						//if not, re enter
+			//if valid, prompt to enter amount
+				//read inputted amount
+					//check if valid
+						//if not, reenter
+			
+		
+	
+		return 0;
+	}
 
 }
