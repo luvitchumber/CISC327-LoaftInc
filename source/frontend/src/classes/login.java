6ec5a02b6bff898ccce79f12884a7ff9c92d3d87
jaaywong <jaaaywong@gmail.com>
2019-10-17 17:00:19 -0400

    Login update checks + send mode/terminal

--- source/frontend/src/classes/login.java
+++ source/frontend/src/classes/login.java
@@ -1,27 +1,20 @@
 package classes;
-import java.io.*; 
+import java.io.*;
+import java.util.ArrayList;
 import java.util.Scanner; 
 
 public class login {
-	static boolean signIn (String args[]) {
-		boolean state = false;
-		//confirm that the account number is valid
-		try {
-	        int accNum = Integer.parseInt(args[0]);
-	    } catch (NumberFormatException e) {
-	        System.err.println("Inputted account number is invalid.");
-	        return state;
-	    }
-		
-		//select agent or atm
-		//if start instance is started, stay logged in
-		return state;
-	}
 	
-	static String loginMode() {
-		System.out.println("Chosen mode: "+args[0]); 
-		String mode = "";
-		String checkType = args[0].toLowerCase();
+	static boolean String loginMode(frontend terminal, File acctsfile) {
+		//1=error
+		
+		//get terminal input
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Select One: (Agent/Machine) ");
+		String mode = scanner.next();
+		
+		System.out.println("Chosen mode: "+ mode); 
+		String checkType = mode.toLowerCase();
 		
 		if (checkType.contains("agent")) {
 			mode = "Agent";
@@ -30,20 +23,26 @@ public class login {
 		}else {
 			mode = "Error";
 			System.err.println("Selected mode is invalid, please select Agent or Machine");
-	        return mode;
+	        return 1;
 		}
 		
-		File file = new File("C:\\valid_accts.txt"); 
-	    Scanner sc = new Scanner(file);
+		ArrayList<String> accts = new ArrayList<String>();
+		
+	    Scanner sc = new Scanner(acctsfile);
 	    String acct = "";
 	    while (sc.hasNextLine()) {
 	    	acct = sc.nextLine();
-	    	if (!validateFile(acct)) {
-	    		return "Accounts list is invalid";
+	    	if (acct == "0000000" && !sc.hasNextLine()) {
+	    		break;
 	    	}
+	    	if (!validateFile(acct)) {
+	    		return 1;
+	    	}
+	    	accts.add(acct);
 	    }
-	    
-		return mode;
+	    terminal.setAccts(accts);
+	    terminal.setMode(mode);
+		return 0;
 	}
 	
 	static boolean validateFile(String acctNum) {
@@ -55,8 +54,8 @@ public class login {
 			return false;
 		}
 		
-		if (acctNum.length() == 7) {
-			
+		if (acctNum.length() != 7 || acctNum.charAt(0) == '0') {
+			return false;
 		}
 			
 		
