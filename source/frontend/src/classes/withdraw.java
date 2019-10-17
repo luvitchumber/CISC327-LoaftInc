a4c7a5fb45509e6f787171548c3401e8647594ba
luvitchumber <luvitchumber@gmail.com>
2019-10-17 17:01:13 -0400

    basic withdraw structure

--- source/frontend/src/classes/withdraw.java
+++ source/frontend/src/classes/withdraw.java
@@ -1,5 +1,13 @@
 package classes;
 
 public class withdraw {
+	
+	public boolean withdrawFromAccount(frontend terminal, String account, double amount) {
+		validAccounts = terminal.getvalidAccts();
+		
+		//if account in validAccounts
+			// if sufficient ammount and not at limit
+		return 0;
+	}
 
 }
