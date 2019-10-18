92a33ce02e66e81dc9a4e99200ded46b14a20fbe
luvitchumber <luvitchumber@gmail.com>
2019-10-18 11:59:31 -0400

    moved input account validation to terminal class

--- source/frontend/src/functions/Terminal.java
+++ source/frontend/src/functions/Terminal.java
@@ -71,4 +71,60 @@ public class Terminal {
 		// TODO Auto-generated method stub
 		return cli;
 	}
+	
+	//check if input is numeric
+	static boolean isNumeric(String str) {
+		try {
+			Integer.parseInt(str);
+			return true;		
+		} 	catch (NumberFormatException e) {
+			return false;
+		}
+	}
+	//check if input starts with 0
+	private static boolean inputStart(String s) {
+		char c = s.charAt(0);
+		if (c==000) {
+			return true;		
+		} else return false;
+	}
+	//check if input length is 7 digits
+	private static boolean inputLength(String s) {
+		if (s.length() > 7) {
+			return true;
+		} else return false;
+	}	
+	//check if account number exists on file
+	private static boolean isValidAccount(ArrayList<String> list, String s) {
+		if (list.contains(s)) {
+			return true;
+		} else return false;
+	}
+	//check if the inputed account number passes all requirements
+	public static boolean accountInputValidation(ArrayList<String> list, String s) {
+			boolean result = true;
+		if (inputStart(s)) {
+			s = "Invalid";
+			System.err.println("Invalid account number: 1st digit cannot be 0");
+			result = false;
+		}
+		
+		else if (!isNumeric(s)) {
+			s = "Invalid";
+			System.err.println("Invalid account number: Please enter numbers only");
+			result = false;
+		}
+		
+		else if (inputLength(s)) {
+			s = "Invalid";
+			System.err.println("Invalid account number: Please enter 7 digits only");
+			result = false;
+		}
+		
+		else if (!isValidAccount(list, s)) {
+			s = "Invalid";
+			System.err.println("Invalid account number: Account number does not exist");
+			result = false;
+		} return result;
+	}
 }
--- source/frontend/src/functions/deposit.java
+++ source/frontend/src/functions/deposit.java
@@ -4,61 +4,6 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class deposit {
-	//check if input is numeric
-	public static boolean isNumeric(String str) {
-		try {
-			Integer.parseInt(str);
-			return true;		
-		} 	catch (NumberFormatException e) {
-			return false;
-		}
-	}
-	//check if input starts with 0
-	public static boolean inputStart(String s) {
-		char c = s.charAt(0);
-		if (c==000) {
-			return true;		
-		} else return false;
-	}
-	//check if input length is 7 digits
-	public static boolean inputLength(String s) {
-		if (s.length() > 7) {
-			return true;
-		} else return false;
-	}	
-	//check if account number exists on file
-	public static boolean isValidAccount(ArrayList<String> list, String s) {
-		if (list.contains(s)) {
-			return true;
-		} else return false;
-	}
-	//check if the inputed account number passes all requirements
-	public static boolean accountInputValidation(ArrayList<String> list, String s) {
-			boolean result = true;
-		if (inputStart(s)) {
-			s = "Invalid";
-			System.err.println("Invalid account number: 1st digit cannot be 0");
-			result = false;
-		}
-		
-		else if (!isNumeric(s)) {
-			s = "Invalid";
-			System.err.println("Invalid account number: Please enter numbers only");
-			result = false;
-		}
-		
-		else if (inputLength(s)) {
-			s = "Invalid";
-			System.err.println("Invalid account number: Please enter 7 digits only");
-			result = false;
-		}
-		
-		else if (!isValidAccount(list, s)) {
-			s = "Invalid";
-			System.err.println("Invalid account number: Account number does not exist");
-			result = false;
-		} return result;
-	}
 	
 	//check if user inputed deposit length is valid
 	public static boolean depositLen(String amount) {
@@ -76,7 +21,7 @@ public class deposit {
 	}
 	//check if account exceeds daily limit
 	public static boolean dailyTransactionLimit (Terminal terminal) {
-		if (terminal.getMode == "ATM" && terminal.transactionTotal > 5000) {
+		if (terminal.getMode() == "ATM" && terminal.transactionTotal() > 5000) {
 			return false;
 		} 
 			else return true; 
@@ -85,7 +30,7 @@ public class deposit {
 	public static boolean depositInputValidation(Terminal terminal, String amount) {
 		boolean result = true;
 		double limit = accountLimit(terminal);
-		if (!isNumeric(amount)) {			
+		if (!Terminal.isNumeric(amount)) {			
 			amount = "Invalid";
 			System.err.println("Invalid amount: Please input a numerical amount");
 			result = false;
@@ -120,7 +65,7 @@ public class deposit {
 		System.out.print("Input Account Number");
 		String accNum = scanner.next();
 		
-		if (accountInputValidation(validAccounts, accNum)) {
+		if (Terminal.accountInputValidation(validAccounts, accNum)) {
 			System.out.print("Input Deposit Amount");
 			String depAmount = scanner.next();
 			if (depositInputValidation(terminal, depAmount)) {
