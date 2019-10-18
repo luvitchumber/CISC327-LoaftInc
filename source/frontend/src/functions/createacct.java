aa654f8222addcf153a9d97814daa7b1f7c36812
luvitchumber <luvitchumber@gmail.com>
2019-10-18 16:00:48 -0400

    updated to fix errors in create and delete account

--- source/frontend/src/functions/createacct.java
+++ source/frontend/src/functions/createacct.java
@@ -5,68 +5,71 @@ import java.util.ArrayList;
 
 public class createacct {
 		
-	public void createAcct(ArrayList<String> accts) {
+	public void createAcct(Terminal terminal) {
 		String acctNum;
 		String acctName;
+		String mode = terminal.getMode();
+		ArrayList<String> accts = terminal.getvalidAccts();
+		Scanner in = terminal.getCLIScanner();
 		
-		checkMode(Terminal.getMode());    // ensure that the user is in agent mode
+		checkMode(mode);    // ensure that the user is in agent mode
 		
-		acctNum = inputAcctNum(accts);    // user will input new account number and program will validate it
-		acctName = inputAcctName();       // user will input new account name and program will validate it
-		addToTSF(acctNum, acctName);      // add the new account name and number to the Transaction Summary File
+		acctNum = inputAcctNum(accts,in);    // user will input new account number and program will validate it
+		acctName = inputAcctName(in);       // user will input new account name and program will validate it
+		addToTSF(terminal, acctNum, acctName);      // add the new account name and number to the Transaction Summary File
 	} // end createAcct
 	
 	
 	// take user input for the account number
-	public String inputAcctNum(ArrayList<String> accts) {
+	public String inputAcctNum(ArrayList<String> accts, Scanner in) {
 		System.out.println("Input new account number.");
-		String acctNum = scanner.next();
-		checkAcctNum(acctNum, accts);       // validate account number
+		String acctNum = in.next();
+		checkAcctNum(acctNum, accts,in);       // validate account number
 		return acctNum;
 	} // end inputAcctMum method
 	
 	
 	// take user input for the account name
-	public String inputAcctName() {
+	public String inputAcctName(Scanner in) {
 		System.out.println("Input new account name.");
-		String acctName = scanner.next();
-		checkAcctName(acctName);     // validate account name
+		String acctName = in.next();
+		checkAcctName(acctName, in);     // validate account name
 		return acctName;
 	} // end inputAcctName method
 	
 	
 	// ensures that the new account number follows restrictions. If it does not, it prompts the user to enter a new account number
-	public void checkAcctNum(String acctNum, ArrayList<String> accts) {
+	public void checkAcctNum(String acctNum, ArrayList<String> accts, Scanner in) {
 		
 		// check that the length of the new account number is 7
 		if (acctNum.length() != 7) {
 			System.out.println("Account number must be 7 digits long");
-			inputAcctNum();
+			inputAcctNum(accts,in);
 		}
 		
 		if (acctNum.charAt(0) == '0') {
-			System.out.println("The first digit in an account number cannot be 0.")
-			inputAcctNum();
+			System.out.println("The first digit in an account number cannot be 0.");
+			inputAcctNum(accts,in);
 		}
 		
 		// check that the account number only contains numerical digits
-		for (int i; i < acctNum.length(); i++) {
+		for (int i = 0; i < acctNum.length(); i++) {
 			if (! Character.isDigit(acctNum.charAt(i))) {
 				System.out.println("Account number must only contain integers.");
-				inputAcctNum();
+				inputAcctNum(accts,in);
 			}
 		} // end for-loop
 		
 		// ensure that the new account number isn't already in use
 		if (accts.contains(acctNum)) {
 			System.out.println("This account number is already in use.");
-			inputAcctNum();
+			inputAcctNum(accts,in);
 		}
 	} // end checkAcctNum method
 
 	
 	// ensures that the new account name follows restrictions. If it does not, it prompts the user to enter a new account name
-	public void checkAcctName(String acctName) {
+	public void checkAcctName(String acctName, Scanner in) {
 		
 		// check that the length of the new account name is between 3 and 30 characters
 		if (acctName.length() < 3 || acctName.length() > 30) {
@@ -76,22 +79,23 @@ public class createacct {
 		// check that the new account name does not start or end with a space
 		if (acctName.charAt(0) == ' ' || acctName.charAt(acctName.length() - 1) == ' ') {
 			System.out.println("Account name cannot start or end with a space");
-			inputAcctName();
+			inputAcctName(in);
 		}
 		
 		// check that every character in the new account name is alphanumeric
-		for (int i; i < acctName.length(); i++) {
-			if (! Character.isDigit(acctName.charAt(i)) && ! Character.isLetter(acctName.charAt(i)) && ! acctName.charAt(i) == ' ') {
+		for (int i = 0; i < acctName.length(); i++) {
+			if (! Character.isDigit(acctName.charAt(i)) && ! Character.isLetter(acctName.charAt(i)) && !(acctName.charAt(i) == ' ')) {
 				System.out.println("The account name can only contain alphanumeric characters.");
-				inputAcctName();
+				inputAcctName(in);
 			}
 		} // end for-loop
 	} // end checkAcctName method
 	
 	// after validating the account name and number, we add it to the transaction summary file
-	public void addToTSF(String acctNum, String acctName) {
+	public Terminal addToTSF(Terminal terminal, String acctNum, String acctName) {
 		String trans = "NEW " + acctNum + " " + acctName;
-		Terminal.addTransaction(trans);
+		terminal.addTransaction(trans);
+		return terminal;
 	} // end addToTSF method
 	
 	
--- source/frontend/src/functions/deleteacct.java
+++ source/frontend/src/functions/deleteacct.java
@@ -5,45 +5,51 @@ import java.util.ArrayList;
 
 public class deleteacct {
 	
-	public void deleteAcct(ArrayList<String> accts) {	
+	public void deleteAcct(Terminal terminal) {	
 		String acctNum;
 		String acctName;
+		String mode = terminal.getMode();
+		ArrayList<String> accts = terminal.getvalidAccts();
+		Scanner in = terminal.getCLIScanner();
 		
-		checkMode(Terminal.getMode());    // ensure that the user is in agent mode
+		checkMode(mode);    // ensure that the user is in agent mode
+		if (mode.equals("atm")) {
+			return;
+		}
 		
-		acctNum = inputAcctNum(accts);    // user will input an account number to delete and program will validate it
-		acctName = inputAcctName();       // user will input the corresponding account name
-		addToTSF(acctNum, acctName);      // add the deleted account name and number to the Transaction Summary File
+		acctNum = inputAcctNum(accts, in);    // user will input an account number to delete and program will validate it
+		acctName = inputAcctName(in);       // user will input the corresponding account name
+		addToTSF(terminal,acctNum, acctName);      // add the deleted account name and number to the Transaction Summary File
 		
 	} // end deleteAcct method
 	
-	public String inputAcctNum(ArrayList<String> accts) {
+	public String inputAcctNum(ArrayList<String> accts, Scanner in) {
 		System.out.println("Input account number to delete.");
-		String acctNum = scanner.next();
-		doesExist(acctNum, accts);       // validate account number
+		String acctNum = in.next();
+		doesExist(acctNum, accts,in);       // validate account number
 		return acctNum;
 	} // end inputAcctMum method
 	
-	public String inputAcctName() {
+	public String inputAcctName(Scanner in) {
 		System.out.println("Input new account name.");
-		String acctName = scanner.next();
+		String acctName = in.next();
 		return acctName;
 	} // end inputAcctName method
 	
 	
 	// check if the account number entered is a valid account
-	public void doesExist(String acctNum, ArrayList<String> accts) {
+	public void doesExist(String acctNum, ArrayList<String> accts, Scanner in) {
 		if (! accts.contains(acctNum)) {
 			System.out.println("This account number does not exist.");
-			inputAcctNum();
+			inputAcctNum(accts, in);
 		}
 	} // end doesExist method
 	
 	
 	// after validating the account name and number, we add it to the transaction summary file
-	public void addToTSF(String acctNum, String acctName) {
+	public void addToTSF(Terminal terminal, String acctNum, String acctName) {
 		String trans = "DEL " + acctNum + " " + acctName;
-		Terminal.addTransaction(trans);
+		terminal.addTransaction(trans);
 	} // end addToTSF method
 	
 	// ensure that the user is in agent mode to delete accounts
