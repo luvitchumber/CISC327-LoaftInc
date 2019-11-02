22111909ccd7f46f0510fc45e378a2db26454ed7
jaaywong <jaaaywong@gmail.com>
2019-11-02 18:21:06 -0400

    add output for create acct

--- source/QUINTERAC/frontend/frontend/frontend.java
+++ source/QUINTERAC/frontend/frontend/frontend.java
@@ -43,8 +43,8 @@ public class frontend {
 				//t = t.addTransaction(cache);
 				break;
 			}else{
-				System.err.println("Selected transaction is unavailable, please login before continuing.");
-				System.err.println("Sample login: login atm or login agent");
+				System.err.println("Error: Selected transaction is unavailable, please login before continuing.");
+				System.err.println("Sample login: 'login atm' or 'login agent'");
 			}
 		}
 		
@@ -75,6 +75,7 @@ public class frontend {
 					if(!exist && !acctNum.equals("NotValid") && !acctName.equals("NotValid")) {
 						cache = new CreateAcct(acctName,acctNum);
 						t=t.addTransaction(cache);
+						System.out.println("Account created.");
 					}
 				}else {
 					System.err.println("Need privileged mode to Create Account");
