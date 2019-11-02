bd89091684e67c2763e5e27b90b1d55cb76f487c
luvitchumber <luvitchumber@gmail.com>
2019-11-01 20:16:28 -0400

    implemented validate functions. Implemented logout, create and delete account structure

--- source/QUINTERAC/frontend/frontend/frontend.java
+++ source/QUINTERAC/frontend/frontend/frontend.java
@@ -4,6 +4,8 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import transactions.CreateAcct;
+import transactions.DeleteAcct;
 import transactions.Login;
 import transactions.Transaction;
 import transactions.Logout;
@@ -24,14 +26,16 @@ public class frontend {
 		
 		
 		// Loop until logged in 
-		while(input[0] != "login") {
+		while(true) {
 			inputRaw = in.nextLine().toLowerCase();
 			input = inputRaw.split(" ");
 			
 			if (input[0] == "login" && (input[1] =="atm" || input[1]=="agent")) {	//only login, if not already logged in
 				t = validateAcctsFile(t, acctsFile);
 				cache = new Login(input[1]);
+				t = t.setMode(input[1]);
 				t = t.addTransaction(cache);
+				break;
 			}else{
 				System.err.println("Selected transaction is unavailable, please login before continuing.");
 				System.err.println("Sample login: login atm or login agent");
@@ -39,7 +43,7 @@ public class frontend {
 		}
 		
 		// Loop until logout
-		while(input[0] != "logout") {
+		while(true) {
 			inputRaw = in.nextLine().toLowerCase();
 			input = inputRaw.split(" ");
 			
@@ -49,19 +53,67 @@ public class frontend {
 				// example: cache = withdraw(name,acctnum,amount
 				// add to file
 				// t = t.addTransaction(cache);
+			if (input[0] == "logout") {
+				//logout function
+				cache = new Logout();
+				t = t.addTransaction(cache);
+				break;
+			}else if (input[0] == "createacct") {
+				//createacct function
+				if (t.getMode() == "agent") {
+					boolean exist = DoesAcctNumExist(t.getAccts(),input[1]);
+					String acctNum=validateAcctNumandReturn(t.getAccts(),input[1]);
+					String acctName=validateNameandReturn(input[2]);
+					
+					if(!exist && acctNum !="NotValid" && acctName != "NotValid") {
+						cache = new CreateAcct(acctName,acctNum);
+						t=t.addTransaction(cache);
+					}
+				}else {
+					System.err.println("Need privileged mode to Create Account");
+				}
+			}else if (input[0] == "deleteacct") {
+				//deleteacct function
+				if (t.getMode() == "agent") {
+					boolean exist = DoesAcctNumExist(t.getAccts(),input[1]);
+					String acctNum=validateAcctNumandReturn(t.getAccts(),input[1]);
+					String acctName=validateNameandReturn(input[2]);
+					
+					if(exist && acctNum !="NotValid" && acctName != "NotValid") {
+						cache = new DeleteAcct(acctName,acctNum);
+						t=t.addTransaction(cache);
+					}
+				}else {
+					System.err.println("Need privileged mode to Delete Account");
+				}
+			}else if (input[0] == "withdraw") {
+				//withdraw function
+				int amount;
+				String acctNum;
+			}else if (input[0] == "deposit") {
+				//deposit function
+				
+			}else if (input[0] == "transfer") {
+				//transfer function
+				
+			}else {
+				//error with input
+				System.err.println("Selected transaction is unavailable, please enter a valid transaction code");
+			}
 		}
 		
-		cache = new Logout();
-		t = t.addTransaction(cache);
+		
 		
 		//create Transaction Summary File
 		createTSF(t, tsfFile);
 	}
 	
-	public static Terminal validateAcctsFile(Terminal t, File accts) {
+	public static Terminal validateAcctsFile(Terminal t, File acctsFile) {
 		//add validate here
 		try {
-			
+			//create ArrayList<String> accts;
+			//validate file and add to accts;
+			//t = t.setAccts(accts);
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -77,19 +129,47 @@ public class frontend {
 		return t;
 	}
 
-	public static int validateNumberandReturn(String in) {
+	public static int validateAmountandReturn(String in) {
 		int num = 0;
 		return num;
 	}
 	
 	public static String validateNameandReturn(String in) {
 		String name = "";
+		
+		if (in.length()>=3 && in.length()<=30) {
+			name=in;
+		}else {
+			name="NotValid";
+		}
+		
 		return name;
 	}
 	
-	public static String validateAcctandReturn(String in) {
+	public static String validateAcctNumandReturn(ArrayList<String> accts, String in) {
 		String acct = "";
+		in = in.trim();
+		if (in.charAt(0)!='0' && in.length()==7) {
+			try {
+				Integer.parseInt(in);
+			}catch(NumberFormatException e){
+				e.printStackTrace();
+			}finally {
+				acct=in;
+			}
+		}else {
+			acct="NotValid";
+		}
+		
 		return acct;
+	}
+	public static boolean DoesAcctNumExist(ArrayList<String> accts, String in) {
+		in = in.trim();
+		
+		if (accts.contains(in)) {
+			return true;
+		}
+		return false;
 	}
 	
 }
