34a5c528e33b146a86ae41e80faf20069c9aa74e
luvitchumber <luvitchumber@gmail.com>
2019-11-05 21:52:07 -0500

    implemented equals and hashcde method to use to check if account is in master accounts

--- source/QUINTERAC/backend/backend/Account.java
+++ source/QUINTERAC/backend/backend/Account.java
@@ -62,11 +62,37 @@ public class Account implements Comparable{
 	}
 
 	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((acct == null) ? 0 : acct.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+
+		boolean sameSame = false;
+		
+		if (obj != null && obj instanceof String) {
+			sameSame = this.acct.equals((String)obj);
+		}
+		
+		return sameSame;
+	}
+
+	/* (non-Javadoc)
 	 * @see java.lang.Object#toString()
 	 */
 	@Override
 	public String toString() {
-		return "";
+		return "Account [acct=" + acct + ", amount=" + amount + ", name=" + name + "]";
 	}
 
 }
