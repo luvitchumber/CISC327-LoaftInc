d22b1d28695235a5ac59900e1606519cb43709c6
luvitchumber <luvitchumber@gmail.com>
2019-11-06 13:09:46 -0500

    implemented transaction method to update master accts file.

--- source/QUINTERAC/backend/backend/backend.java
+++ source/QUINTERAC/backend/backend/backend.java
@@ -2,14 +2,19 @@ package backend;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Scanner;
 
-import frontend.Terminal;
+import transactions.Transaction;
 
 public class backend {
 
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		// read previous master accounts file
 		// read in merged tsf 
@@ -20,10 +25,35 @@ public class backend {
 		// produce updated valid accts file
 		
 		File masterAcctsFile = new File(args[0]);
-		ArrayList<Account> masterAccts= validateMasterAcctsFile(masterAcctsFile);
+		String[] mergedTsfFileNames = Arrays.copyOfRange(args, 1, args.length); // in case of multiple TSFs
+		
+		ArrayList<Account> masterAccts = validateMasterAcctsFile(masterAcctsFile);
+		
+		masterAccts = updateMasterAccts(masterAccts,mergedTsfFileNames);
+		
+		Comparator<Account> c = Collections.reverseOrder(); //sort descending order
+		Collections.sort(masterAccts,c);
+		
+		createNewMasterAcctsFile(masterAcctsFile,masterAccts);
+		
 		System.out.println(masterAccts.toString());
 	}
-	
+
+	private static void createNewMasterAcctsFile(File masterAcctsFile, ArrayList<Account> masterAccts) throws IOException {
+		// TODO Auto-generated method stub
+		
+		String output = "";
+		for (int i = 0; i < masterAccts.size(); i++) {
+			Account cache = masterAccts.get(i);
+			if(cache!=null) {
+				output += cache.toString() + "\n";
+			}
+		}
+		
+		Files.write(masterAcctsFile.toPath(),output.getBytes());
+		
+	}
+
 	private static ArrayList<Account> validateMasterAcctsFile(File masterAcctsFile) throws FileNotFoundException {
 		//add validate here
 		ArrayList<Account> masterAccts = new ArrayList<Account>();		//create ArrayList<String> accts;
@@ -34,7 +64,9 @@ public class backend {
 		while (sc.hasNextLine()) {		//while there is another line to parse through
 	    	inRaw = sc.nextLine();
 	    	
-	    	String[] in = inRaw.split(" "); // cannot use this method for names, can have spaces
+	    	// add check if line is correct length else error
+	    	
+	    	String[] in = inRaw.split(" "); // cannot use this method for names, names can have spaces
 	    	String acct = in[0];
 	    	String amountStr = in[1];
 	    	int startIDX = inRaw.lastIndexOf(amountStr);
@@ -61,6 +93,126 @@ public class backend {
     	}
 	    sc.close();	//close scanner
 
+		return masterAccts;
+	}
+	
+	private static ArrayList<Account> updateMasterAccts(ArrayList<Account> masterAccts, String[] mergedTsfFileNames) throws FileNotFoundException {
+		// TODO Auto-generated method stub
+		for(int i = 0; i < mergedTsfFileNames.length; i++) {
+			File tsf = new File(mergedTsfFileNames[i]);
+			Scanner sc = new Scanner(tsf);
+			
+			while (sc.hasNextLine()) {
+				String inRaw = sc.nextLine();
+				if (inRaw.equals("EOS") || !sc.hasNextLine()) {
+					continue; // if eos or no more lines, leave current iteration.
+				}
+				
+				String[] in = inRaw.split(" "); // cannot use this method for names, names can have spaces
+		    	String type = in[0];
+		    	String acctTo = in[1];
+		    	String amountStr = in[2];
+		    	String acctFrom = in[3];
+		    	int startIDX = inRaw.lastIndexOf(acctFrom);
+		    	String name = inRaw.substring(startIDX+acctFrom.length());
+		    	name = name.trim(); // removes space from front of string;	
+		    	
+		    	//validate all fields here ELSE FATAL ERROR;
+		    	name = validateNameandReturn(name);
+		    	acctTo = validateAcctNumandReturn(acctTo);
+		    	acctFrom = validateAcctNumandReturn(acctFrom);
+		    	int amount = validateAmountandReturn(amountStr);
+		    	
+		    	switch(type) {
+		    		case "NEW":
+		    			masterAccts = transaction(masterAccts,type,acctTo,name,amount);
+		    			break;
+		    		case "DEL":
+		    			masterAccts = transaction(masterAccts,type,acctFrom,name,amount);
+		    			break;
+		    		case "DEP":
+		    			masterAccts = transaction(masterAccts,type,acctTo,name,amount);
+		    			break;
+		    		case "WDR":
+		    			masterAccts = transaction(masterAccts,type,acctFrom,name,amount);
+		    			break;
+		    		case "XFR":
+		    			if (DoesAcctNumExist(masterAccts,acctFrom) && DoesAcctNumExist(masterAccts,acctTo)) { // not needed? checks and throws in transaction...
+		    				try {
+		    					masterAccts = transaction(masterAccts,"WDR",acctFrom,name,amount); //withdraw first to make sure there are sufficent resources
+		    					masterAccts = transaction(masterAccts,"DEP",acctTo,name,amount);
+	    					} catch (IllegalArgumentException e) {
+	    						//e.printStackTrace();
+	    						System.err.println(e.getMessage());
+	    					}
+		    			}else {
+//		    				error
+		    			}
+		    			break;
+	    			default:
+	    				//throw fatal error here?? only way to reach is if inccorect type code.
+		    	}
+		    	
+			}
+			sc.close();
+			
+		}
+		
+		return masterAccts;
+	}
+	
+	private static ArrayList<Account> transaction(ArrayList<Account> masterAccts,String type,String acct,String name,int amount) throws IllegalArgumentException {
+//		check if account exists else throw fatal error
+		if (!type.equals("NEW") && !DoesAcctNumExist(masterAccts,acct)) {
+			//throw error
+		}
+		
+		Account temp = new Account(acct,amount,name);
+		
+		if(!name.equals("NotValid")){
+			switch(type) {
+				case "NEW":
+					temp = new Account(acct, 0, name);
+					masterAccts.add(temp);
+					break;
+				case "DEL":
+					if (masterAccts.contains(acct)){// needed? checks at the top...// implemented equals override to check based on account number
+						int idx = masterAccts.indexOf(acct);
+						temp = masterAccts.get(idx);
+						if(temp.getAmount() == 0 && temp.getName().equals(name)) {
+							masterAccts.remove(idx);
+						}else {
+//							error
+						}
+					}
+					break;
+			}
+		}
+		
+		if(amount != -1){
+			switch(type) {
+				case "DEP":
+					int idx = masterAccts.indexOf(temp);
+					temp = masterAccts.get(idx);
+					int currentAmount = temp.getAmount();
+					temp.setAmount(currentAmount+amount);
+					masterAccts.set(idx, temp);
+					break;
+				case "WDR":
+					idx = masterAccts.indexOf(temp);
+					temp = masterAccts.get(idx);
+					currentAmount = temp.getAmount();
+					if (amount<currentAmount) {
+						temp.setAmount(currentAmount-amount);
+						masterAccts.set(idx, temp);
+					}else {
+//						error
+						throw new IllegalArgumentException("Insufficent funds to transfer");
+					}
+					break;
+			}
+		}
+		
 		return masterAccts;
 	}
 	
@@ -125,8 +277,9 @@ public class backend {
 	//returns true if account is valid
 	private static boolean DoesAcctNumExist(ArrayList<Account> accts, String in) {
 		in = in.trim();
+		Account cache = new Account(in,-1,null);
 		
-		if (accts.contains(in)) {
+		if (accts.contains(cache)) {
 			return true;
 		}
 		return false;
