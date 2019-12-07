e6d4dcdc6371ca010f6a0380a9dcc58f6a20f8fd
luvitchumber <luvitchumber@gmail.com>
2019-12-07 01:01:26 -0500

    added step to clear accounts files for fresh start. Updated transactions for clean execution

--- source/QUINTERAC/Testing/integration/integrationTest.java
+++ source/QUINTERAC/Testing/integration/integrationTest.java
@@ -14,7 +14,6 @@ import java.io.PrintStream;
 import java.nio.file.Files;
 import java.util.List;
 import java.util.Scanner;
-
 import frontend.frontend;
 import backend.backend;
 
@@ -51,6 +50,12 @@ public class integrationTest {
 	    System.setOut(outContent);
 	    System.setErr(errContent);
 	    
+	    // reset files to clean slate
+	    File mAccts = new File(System.getProperty("user.dir")+"\\files\\masteraccts.txt");
+	    File vAccts = new File(System.getProperty("user.dir")+"\\files\\validaccts.txt");
+	    Files.write(mAccts.toPath(),"".getBytes());
+	    Files.write(vAccts.toPath(),"0000000".getBytes()); 
+	    
 	    String t_in_day1[][] = { 
 	    	{ "login agent",          //session 1
 			  "createacct 1000000 TayyabA",
@@ -69,19 +74,19 @@ public class integrationTest {
 			  "logout"} };                   //END DAY 1
 			 
 	    String t_in_day2[][] = { 
-	    	{ "login atm",	        //session 1
+	    	{ "login agent",	        //session 1
 		      "deposit 1000000 50000",
 			  "deposit 1000001 100",
 			  "deposit 9999999 200000",
 			  "logout"}, 
-	    	{ "login atm",                  //session 2
+	    	{ "login agent",                  //session 2
 			  "deposit 9999999 9500000",
 			  "deposit 5000001 320",
 			  "deposit 8300781 100000",
 			  "deposit 3030303 671000",
 			  "deposit 1000002 500",
 			  "logout"}, 
-	    	{ "login atm",                  //session 3
+	    	{ "login agent",                  //session 3
 			  "deposit 7777777 8000000",
 			  "deposit 3030303 500000",
 			  "deposit 1000003 1500",
@@ -93,25 +98,25 @@ public class integrationTest {
 			  "transfer 1000000 1000001 1000",
 			  "transfer 1000000 1000002 1000",
 			  "logout"}, 
-	    	{ "login atm",                  //session 2
+	    	{ "login agent",                  //session 2
 			  "transfer 7777777 1000000 50000",
-			  "transfer 3030303 8300781 70000",
+			  "transfer 3030303 8300781 7000",
 			  "transfer 3030303 9999999 100000",
 			  "transfer 5000001 1000003 320",
 			  "logout"}, 
-	    	{ "login atm",                  //session 3
+	    	{ "login agent",                  //session 3
 			  "transfer 7777777 1000003 50000",
-			  "transfer 9999999 1000000 1500000",
+			  "transfer 9999999 1000000 150000",
 			  "transfer 9999999 1000001 100000",
 			  "transfer 9999999 1000002 10000",
 			  "transfer 9999999 1000003 10000",
 			  "logout"} };				     //END DAY 3
 			  
 	    String t_in_day4[][] = { 
-	    	{ "login atm",          //session 1
+	    	{ "login agent",          //session 1
 			  "withdraw 9999999 8180000", 
 			  "logout"}, 
-	    	{ "login atm",                  //session 2
+	    	{ "login agent",                  //session 2
 			  "withdraw 1000001 101100",
 			  "withdraw 5000001 400000",
 			  "withdraw 1000003 61820",
@@ -119,26 +124,28 @@ public class integrationTest {
 			  "withdraw 1000000 98000",
 			  "withdraw 7777777 7900000",
 			  "logout"}, 
-	    	{ "login atm",                  //session 3
-			  "withdraw 3030303 1001000",
-			  "withdraw 8300781 170000",
+	    	{ "login agent",                  //session 3
+			  "withdraw 3030303 1064000",
+			  "withdraw 8300781 107000",
+			  "withdraw 1000000 150000",
 			  "logout"} };				     //END DAY 4
 			  
 	    String t_in_day5[][] = { 
 	    	{ "login agent",		    //session 1
-			  "deleteacct 1000000 TayyabA",
-			  "deleteacct 1000001 LuvitC",
 			  "deleteacct 5000001 JaneDee69",
+			  "transfer 9999999 1000000 100",
+			  "transfer 9999999 1000001 100",
+			  "withdraw 9999999 1349600",
 			  "logout"}, 
 	    	{ "login agent",                      //session 2
 			  "deleteacct 7777777 BethanyWhite212",
+			  "transfer 9999999 1000003 100",
 			  "deleteacct 3030303 Walter101Grey",
-			  "deleteacct 9999999 JohnDoe420",
+			  "transfer 9999999 1000002 100",
 			  "logout"}, 
 	    	{ "login agent",                  //session 3
-			  "deleteacct 1000002 SamirM",
-			  "deleteacct 1000003 JessicaW",
 			  "deleteacct 8300781 747JonathanBlue",
+			  "deleteacct 9999999 JohnDoe420",
 			  "logout"} };					 //END DAY 5
 	   
 	    
@@ -213,7 +220,7 @@ public class integrationTest {
 		    System.setOut(console);
 		    
 		    // Print Debug Report Each Day
-		    File mAccts = new File(System.getProperty("user.dir")+"\\files\\masteraccts.txt");
+		    mAccts = new File(System.getProperty("user.dir")+"\\files\\masteraccts.txt");
 		    
 		    System.out.println("Day " + (i+1));
 		    System.out.println("> Tranactions being issued");
@@ -237,6 +244,12 @@ public class integrationTest {
 		    
 		    System.out.println("\n");
 		    System.setOut(outContent);
+//		    try {
+//				TimeUnit.SECONDS.sleep(1);
+//			} catch (InterruptedException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
 	  }
 	}
 }
