9fdae116b1d7ebc7ccc61ae8bf49e7d0c0b2d666
luvitchumber <luvitchumber@gmail.com>
2019-12-07 13:52:32 -0500

    refactored code and optimized

--- source/QUINTERAC/Testing/integration/integrationTest.java
+++ source/QUINTERAC/Testing/integration/integrationTest.java
@@ -12,15 +12,11 @@ import java.io.File;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.nio.file.Files;
-import java.util.List;
 import java.util.Scanner;
 import frontend.frontend;
 import backend.backend;
 
 public class integrationTest {
-	static List<String> terminal_input;
-    static List<String> valid_accounts;
-    static List<String> master_accounts;
 
     private static String runFrontEndDaily(String[][] args,String [][] transactions) throws IOException {
     	String tsf_merged = "";
@@ -148,87 +144,70 @@ public class integrationTest {
 			  "deleteacct 9999999 JohnDoe420",
 			  "logout"} };					 //END DAY 5
 	   
+	    String t_in[][][] = {t_in_day1,t_in_day2,t_in_day3,t_in_day4,t_in_day5};
 	    
-	    for (int i = 0; i < 5 ; i++) {
-	    	String [][] t_in = null;
-	    	String tsfname1="";
-	    	String tsfname2="";
-	    	String tsfname3="";
-	    	String mergedtsfname = "";
-	    	
-	    	switch(i) {
-	    	case 1:
-	    		t_in=t_in_day2;
-	    		tsfname1 = System.getProperty("user.dir")+"\\files\\tsfday2_instance1.txt";
-	    		tsfname2 = System.getProperty("user.dir")+"\\files\\tsfday2_instance2.txt";
-	    		tsfname3 = System.getProperty("user.dir")+"\\files\\tsfday2_instance3.txt";
-	    		mergedtsfname = System.getProperty("user.dir")+"\\files\\mergedtsfday2.txt";
-	    		break;
-	    	case 2:
-	    		t_in=t_in_day3;
-	    		tsfname1 = System.getProperty("user.dir")+"\\files\\tsfday3_instance1.txt";
-	    		tsfname2 = System.getProperty("user.dir")+"\\files\\tsfday3_instance2.txt";
-	    		tsfname3 = System.getProperty("user.dir")+"\\files\\tsfday3_instance3.txt";
-	    		mergedtsfname = System.getProperty("user.dir")+"\\files\\mergedtsfday3.txt";
-	    		break;
-	    	case 3:
-	    		t_in=t_in_day4;
-	    		tsfname1 = System.getProperty("user.dir")+"\\files\\tsfday4_instance1.txt";
-	    		tsfname2 = System.getProperty("user.dir")+"\\files\\tsfday4_instance2.txt";
-	    		tsfname3 = System.getProperty("user.dir")+"\\files\\tsfday4_instance3.txt";
-	    		mergedtsfname = System.getProperty("user.dir")+"\\files\\mergedtsfday4.txt";
-	    		break;
-	    	case 4:
-	    		t_in=t_in_day5;
-	    		tsfname1 = System.getProperty("user.dir")+"\\files\\tsfday5_instance1.txt";
-	    		tsfname2 = System.getProperty("user.dir")+"\\files\\tsfday5_instance2.txt";
-	    		tsfname3 = System.getProperty("user.dir")+"\\files\\tsfday5_instance3.txt";
-	    		mergedtsfname = System.getProperty("user.dir")+"\\files\\mergedtsfday5.txt";
-	    		break;
-    		default:
-	    		t_in=t_in_day1;
-	    		tsfname1 = System.getProperty("user.dir")+"\\files\\tsfday1_instance1.txt";
-	    		tsfname2 = System.getProperty("user.dir")+"\\files\\tsfday1_instance2.txt";
-	    		tsfname3 = System.getProperty("user.dir")+"\\files\\tsfday1_instance3.txt";
-	    		mergedtsfname = System.getProperty("user.dir")+"\\files\\mergedtsfday1.txt";
-	    		break;
-	    	}
-	  
+	 // declare all filenames
+	    String mTsfFN[] = 		{	System.getProperty("user.dir")+"\\files\\mergedtsfday1.txt",
+						    		System.getProperty("user.dir")+"\\files\\mergedtsfday2.txt",
+						    		System.getProperty("user.dir")+"\\files\\mergedtsfday3.txt",
+						    		System.getProperty("user.dir")+"\\files\\mergedtsfday4.txt",
+						    		System.getProperty("user.dir")+"\\files\\mergedtsfday5.txt"};
+	    
+	    String tsfFN[][] = 	  {	{ System.getProperty("user.dir")+"\\files\\tsfday1_instance1.txt",
+	    							System.getProperty("user.dir")+"\\files\\tsfday1_instance2.txt", 
+	    							System.getProperty("user.dir")+"\\files\\tsfday1_instance3.txt"},
+	    						{ System.getProperty("user.dir")+"\\files\\tsfday2_instance1.txt",
+		    						System.getProperty("user.dir")+"\\files\\tsfday2_instance2.txt", 
+		    						System.getProperty("user.dir")+"\\files\\tsfday2_instance3.txt"},
+    							{ System.getProperty("user.dir")+"\\files\\tsfday3_instance1.txt",
+		    						System.getProperty("user.dir")+"\\files\\tsfday3_instance2.txt", 
+		    						System.getProperty("user.dir")+"\\files\\tsfday3_instance3.txt"},
+    							{ System.getProperty("user.dir")+"\\files\\tsfday4_instance1.txt",
+		    						System.getProperty("user.dir")+"\\files\\tsfday4_instance2.txt", 
+		    						System.getProperty("user.dir")+"\\files\\tsfday4_instance3.txt"},
+    							{ System.getProperty("user.dir")+"\\files\\tsfday5_instance1.txt",
+		    						System.getProperty("user.dir")+"\\files\\tsfday5_instance2.txt", 
+		    						System.getProperty("user.dir")+"\\files\\tsfday5_instance3.txt"}};
+	   
+	    String vAcctsFN = System.getProperty("user.dir")+"\\files\\validaccts.txt";
+	    String mAcctsFN = System.getProperty("user.dir")+"\\files\\masteraccts.txt";
+	    
+	    for (int i = 0; i < 5 ; i++) {	  
 	    	// create temporary files for tsf transaction
-		    File transaction_summary_file1 = new File(tsfname1);
-		    File transaction_summary_file2 = new File(tsfname2);
-		    File transaction_summary_file3 = new File(tsfname3);
+		    File transaction_summary_file1 = new File(tsfFN[i][0]);
+		    File transaction_summary_file2 = new File(tsfFN[i][1]);
+		    File transaction_summary_file3 = new File(tsfFN[i][2]);
 		    
 		    // setup parameters for the program to run
-		    String[][] args_to_frontend = { { System.getProperty("user.dir")+"\\files\\validaccts.txt", transaction_summary_file1.getAbsolutePath() },
-		    							{ System.getProperty("user.dir")+"\\files\\validaccts.txt", transaction_summary_file2.getAbsolutePath() },
-		    							{ System.getProperty("user.dir")+"\\files\\validaccts.txt", transaction_summary_file3.getAbsolutePath() }};
+		    String[][] args_to_frontend = { { vAcctsFN, transaction_summary_file1.getAbsolutePath() },
+		    								{ vAcctsFN, transaction_summary_file2.getAbsolutePath() },
+		    								{ vAcctsFN, transaction_summary_file3.getAbsolutePath() }};
 		    
 		    
-		    // run frontend (runs 3 instances)
-		    String tsf_merged = runFrontEndDaily(args_to_frontend, t_in);
+		    // run frontend (3 instances)
+		    String tsf_merged = runFrontEndDaily(args_to_frontend, t_in[i]);
 		    
 		    //prepare merged tsf
-		    File mergedtsf = new File(mergedtsfname);
+		    File mergedtsf = new File(mTsfFN[i]);
 		    Files.write(mergedtsf.toPath(), String.join("\n", tsf_merged).getBytes());
 		    
 		    // run backend
-		    String [] args_to_backend = { System.getProperty("user.dir")+"\\files\\masteraccts.txt", mergedtsf.getAbsolutePath()};
+		    String [] args_to_backend = { mAcctsFN, mergedtsf.getAbsolutePath()};
 		    
 		    backend.main(args_to_backend);
 		    
 		    System.setOut(console);
 		    
 		    // Print Debug Report Each Day
-		    mAccts = new File(System.getProperty("user.dir")+"\\files\\masteraccts.txt");
+		    mAccts = new File(mAcctsFN);
 		    
 		    System.out.println("Day " + (i+1));
 		    System.out.println("> Tranactions being issued");
 		    
-		    for(int j = 0; j<t_in.length;j++) {
+		    for(int j = 0; j<t_in[i].length;j++) {
 		    	System.out.println(">> FrontEnd #" + (j+1));
-		    	for(int k = 0; k<t_in[j].length; k++) {
-		    		System.out.println(t_in[j][k]);
+		    	for(int k = 0; k<t_in[i][j].length; k++) {
+		    		System.out.println(t_in[i][j][k]);
 		    	}
 		    }
 		    
@@ -244,12 +223,6 @@ public class integrationTest {
 		    
 		    System.out.println("\n");
 		    System.setOut(outContent);
-//		    try {
-//				TimeUnit.SECONDS.sleep(1);
-//			} catch (InterruptedException e) {
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
-//			}
 	  }
 	}
 }
